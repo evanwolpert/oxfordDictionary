9fb5c9781567797e088b0dead233e23984773296
Evan Wolpert <c20ew@dalton.org>
2018-03-06 11:07:21 -0500

    made changes

--- src/test/GuessingGame.java
+++ src/test/GuessingGame.java
@@ -1,11 +1,30 @@
 package test;
 
+import app.Dictionary;
+
+import java.util.Scanner;
+
 public class GuessingGame {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String word = "helloooo";
+        String letter = "";
+        String userLetter = "";
         // Make a guessing game using the Dictionary Library. See Dictionary Tester for inspiration.
 
         // You can be inventive!
-    }
 
-}
+        do {
+            word = Dictionary.getRandomWord();
+            System.out.println(word);
+
+        } while (word.length() > 6);
+
+        System.out.println("select four letters");
+        userLetter = scan.next();
+
+        while userLetter = letter {
+            System.out.println(userLetter + " is in word");
+        }
+    }
--- src/test/PasswordMaker.java
+++ src/test/PasswordMaker.java
@@ -1,12 +1,25 @@
 package test;
 
+import app.Dictionary;
 public class PasswordMaker {
 
     public static void main(String[] args) {
+        String combined = "hellooooooooooo";
+
+
         // Use the Dictionary library to create a secure password.
 
         // 1. Get 2 random words and put them together.
-        // 1a. Make sure it is at least 8 characters long. If not, do it again.
+
+
+        do {
+            // 1a. Make sure it is at least 8 characters long. If not, do it again.
+            String word = Dictionary.getRandomWord();
+            String Otherword = Dictionary.getRandomWord();
+            combined = word + Otherword;
+
+
+        } while (combined.length() > 8);
 
         // 2. Replace the vowels with numbers following these rules:
         //    a becomes 4
@@ -14,11 +27,21 @@ public class PasswordMaker {
         //    i becomes 1
         //    0 becomes 0
         //    u becomes 6
+        combined = combined.replaceAll("a","4");
+        combined = combined.replaceAll("e","3");
+        combined = combined.replaceAll("i","1");
+        combined = combined.replaceAll("0","o");
+        combined = combined.replaceAll("u","6");
 
         // 3. Capitalize the first letter
+        char firstletter = combined.charAt(0);
+        String cap = String.valueOf(firstletter);
+        String cap2 = cap.toUpperCase();
+        combined = combined.replaceFirst(cap, cap2);
 
-        // 4. Tell the person their password.
+        System.out.println(combined);
 
         // BONUS 5. Ask the person if they want another one and make another one.
     }
 }
+
